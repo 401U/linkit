ac5a5c2027260df188f390aa3a89f60586b9ae14
401Unauthorized <hi@4o1.to>
2023-03-18 02:32:38 +0800

    chore: replace local url

--- middleware/redirect.ts
+++ middleware/redirect.ts
@@ -1,12 +1,12 @@
 export default defineNuxtRouteMiddleware(async (to, _) => {
   if (process.server)
     return
-  if (to.path.startsWith('/link_miss'))
+  if (to.path.startsWith('/redirect_error'))
     return
 
   const data = await $fetch(`/api/links/${to.path}`)
   if (data.code === 302)
     return navigateTo(data.url, { external: true })
   else
-    return navigateTo(`http://localhost:3000/link_miss?code=${data.code}&url=${data.url}`, { external: true })
+    return navigateTo(`/redirect_error?code=${data.code}&url=${data.url}`)
 })
